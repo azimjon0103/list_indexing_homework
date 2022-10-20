7e902ca7b02bc64c24ca5689680a53f4a413cedf
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-20 21:57:34 +0500

    10

--- List10.py
+++ List10.py
@@ -6,4 +6,8 @@ def main(list_num):
     Returns:
         int: return answer
     """
-    return
+    if list_num[0]>list_num[-1]:
+        return int(list_num[0])
+    else:
+        return int(list_num[-1])
+print(main([5,2,3,4,-8]))
